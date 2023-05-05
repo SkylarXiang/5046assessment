cb7f36dbacdff6594b33b051bda1066466bac4c9
Yuxing Ding <1175946849@qq.com>
2023-05-06 00:09:26 +1000

    recipe detail screen setup

--- app/src/main/java/com/example/a5046assessment/RecipeDetailActivity.java
+++ app/src/main/java/com/example/a5046assessment/RecipeDetailActivity.java
@@ -3,13 +3,27 @@ package com.example.a5046assessment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.a5046assessment.databinding.ActivityRecipeDetailBinding;
+import com.squareup.picasso.Picasso;
 
 public class RecipeDetailActivity extends AppCompatActivity {
+    private ActivityRecipeDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe_detail);
+        binding = ActivityRecipeDetailBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         Recipe recipe = (Recipe) getIntent().getSerializableExtra("recipe");
+
+
+        binding.Name.setText(recipe.getStrMeal());
+
+        Picasso.get()
+                .load(recipe.getStrMealThumb())
+                .into(binding.Image);
     }
 }
