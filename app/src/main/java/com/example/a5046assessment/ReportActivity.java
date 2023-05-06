249d507dfe277ddc05f647f3c19051e306ca86ac
XIAOXIGUA599 <116609237+XIAOXIGUA599@users.noreply.github.com>
2023-05-06 10:23:26 +1000

    ReportActivity

--- app/src/main/java/com/example/a5046assessment/ReportActivity.java
+++ app/src/main/java/com/example/a5046assessment/ReportActivity.java
@@ -3,12 +3,18 @@ package com.example.a5046assessment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.a5046assessment.databinding.ActivityReportBinding;
 
 public class ReportActivity extends AppCompatActivity {
+    private ActivityReportBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_report);
+        binding = ActivityReportBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
