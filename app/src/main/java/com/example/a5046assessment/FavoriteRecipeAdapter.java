ee10f00ee73f983074401ac5deff25e9f155ac0c
DESKTOP-R370A58\aufb <jxia0025@student.monash.edu>
2023-05-12 21:51:32 +1000

    remove bottom finished

--- app/src/main/java/com/example/a5046assessment/FavoriteRecipeAdapter.java
+++ app/src/main/java/com/example/a5046assessment/FavoriteRecipeAdapter.java
@@ -4,8 +4,10 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,6 +15,8 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FavoriteRecipeAdapter extends RecyclerView.Adapter<FavoriteRecipeAdapter.FavoriteRecipeViewHolder> {
     private List<FavoriteRecipe> favoriteRecipes = new ArrayList<>();
@@ -30,6 +34,16 @@ public class FavoriteRecipeAdapter extends RecyclerView.Adapter<FavoriteRecipeAd
         FavoriteRecipe currentRecipe = favoriteRecipes.get(position);
         holder.recipeNameTextView.setText(currentRecipe.getRecipeName());
         Picasso.get().load(currentRecipe.getImageUrl()).into(holder.recipeImageView);
+        holder.removeButton.setOnClickListener(v -> {
+            FavoriteRecipe toRemove = favoriteRecipes.get(position);
+            favoriteRecipes.remove(position);
+            notifyItemRemoved(position);
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.execute(() -> {
+                AppDatabase.getInstance(holder.itemView.getContext()).favoriteRecipeDao().delete(toRemove);
+                holder.itemView.post(() -> Toast.makeText(holder.itemView.getContext(), "Recipe removed from favorites", Toast.LENGTH_SHORT).show());
+            });
+        });
     }
 
     @Override
@@ -45,11 +59,13 @@ public class FavoriteRecipeAdapter extends RecyclerView.Adapter<FavoriteRecipeAd
     static class FavoriteRecipeViewHolder extends RecyclerView.ViewHolder {
         private TextView recipeNameTextView;
         private ImageView recipeImageView;
+        private ImageButton removeButton;
 
         public FavoriteRecipeViewHolder(@NonNull View itemView) {
             super(itemView);
             recipeNameTextView = itemView.findViewById(R.id.recipe_name);
             recipeImageView = itemView.findViewById(R.id.recipe_image);
+            removeButton = itemView.findViewById(R.id.remove_button);
         }
     }
 }
